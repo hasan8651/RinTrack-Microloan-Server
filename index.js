eceffee589a645b8d33c51e6ac7edb85d0e02abc
hasan8651 <hasan865@gmail.com>
2025-12-06 23:33:14 +0600

    DB create and checking users collection with GET

--- index.js
+++ index.js
@@ -1,46 +1,38 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
 const app = express();
-
 const port = process.env.PORT || 5000;
-
-
 
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbonu5x.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-        // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
 
-    
     const db = client.db("RinTrack");
-   const usersCollection = db.collection('users')
+    const usersCollection = db.collection("users");
 
     app.get("/users", async (req, res) => {
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -50,26 +42,12 @@ async function run() {
   }
 }
 
-
-
-
-
-
-
-
-
-
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('RinTrack server is running')
-})
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("RinTrack server is running");
+});
 
 app.listen(port, () => {
-    console.log(`RinTrack server is running on port: ${port}`)
-})
+  console.log(`RinTrack server is running on port: ${port}`);
+});
